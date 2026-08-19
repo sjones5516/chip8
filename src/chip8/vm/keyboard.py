4cbdbb721f1c7900e75b09079a87299ef7bf5893
Samuel Jones <sjones5516@floridapoly.edu>
2026-08-19 16:01:26 -0400

    setup keystate type defs

--- src/chip8/vm/keyboard.py
+++ src/chip8/vm/keyboard.py
@@ -1,22 +1,28 @@
-
 from threading import Lock
+from typing import Literal
 
 import numpy as np
+
+type KEYBOARD_SIZE_TYPE = Literal[16]
+KEYBOARD_SIZE = 16
+type Keystate = np.ndarray[
+    tuple[KEYBOARD_SIZE_TYPE, Literal[1]], np.dtype[np.bool_]
+]
 
 
 class Keyboard:
     """Threadsafe keyboard for VM"""
 
-    def __init__(self, size: int) -> None:
-        self._keyboard= np.zeros((size, 1), dtype=bool)
+    def __init__(self) -> None:
+        self._keyboard: Keystate = np.zeros((KEYBOARD_SIZE, 1), dtype=bool)
         self._lock = Lock()
 
     @property
-    def keyboard(self):
+    def keyboard(self) -> Keystate:
         with self._lock:
             return self._keyboard
 
     @keyboard.setter
-    def keyboard(self, value):
+    def keyboard(self, value: Keystate):
         with self._lock:
             self._keyboard = value
