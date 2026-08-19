5f54c5f03116599f6591de7cf50ead76936c7789
Samuel Jones <sjones5516@floridapoly.edu>
2026-08-19 14:44:24 -0400

    load rom impl

--- src/chip8/vm/virtualmachine.py
+++ src/chip8/vm/virtualmachine.py
@@ -11,6 +11,8 @@ class VirtualMachine(VirtualMachineABC):
     NUM_KEYS = 16
     SCREEN_WIDTH = 64
     SCREEN_HEIGHT = 32
+    PROGRAM_START = 0x200
+    AVAILABLE_ROM_LOCATIONS = MEMORY_LOCATIONS - PROGRAM_START
 
     def __init__(self):
         super().__init__()
@@ -24,3 +26,16 @@ class VirtualMachine(VirtualMachineABC):
         self._keystate = [False] * self.NUM_KEYS
         self._screen = Screen(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         self._sound_event = Event()
+
+    def _can_fit_into_memory(self, data_size: int) -> bool:
+        return data_size <= self.AVAILABLE_ROM_LOCATIONS
+
+    def load_rom(self, rom: bytes):
+        rom_array = bytearray(rom)
+        rom_size = len(rom_array)
+        if not self._can_fit_into_memory(rom_size):
+            raise MemoryError(
+                f"Cannot fit ROM into VM memory. Got {rom_size} bytes, max is {self.AVAILABLE_ROM_LOCATIONS}"
+            )
+
+        self._memory[self.PROGRAM_START : self.PROGRAM_START + rom_size] = rom_array
